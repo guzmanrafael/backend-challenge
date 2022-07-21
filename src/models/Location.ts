65d8d0c1106473786aa51bac75fc2d0441ae08c0
Rafael Guzman <rafael.guzman1609@gmail.com>
2022-07-21 09:58:23 -0700

    Add location model

---
+++ src/models/Location.ts
@@ -0,0 +1,15 @@
+export class Location {
+    id!: number;
+  
+    name!: string;
+  
+    type!: string;
+  
+    dimension!: string;
+
+    residents!: string[];
+    
+    url!: string;
+  
+    created!: string;
+  }
